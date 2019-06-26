3b489f3d2f2962d60607fbdeaf5c0540710b536e
amlagsac <andremlagsac@gmail.com>
2019-06-26 09:17:32 +0800

    ADDED MEDICAL ACTIVITY

---
+++ app/src/main/java/com/example/centrilink/MedicalHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.centrilink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_history);
+    }
+}
