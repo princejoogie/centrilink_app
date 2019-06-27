079a80990a899afc56036871eae09fc9511160cd
princejoogie <princejoogie@gmail.com>
2019-06-27 11:34:47 +0800

    Ewan

--- app/src/main/java/com/example/centrilink/ProfileActivity.java
+++ app/src/main/java/com/example/centrilink/ProfileActivity.java
@@ -8,16 +8,16 @@ import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Button logout;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        logout = findViewById(R.id.buttonProfileBack);
+        back = findViewById(R.id.buttonProfileBack);
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
